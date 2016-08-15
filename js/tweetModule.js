fe06c177b34ce7a8e111ed5cc8686838b2ad1e4f
Daniel Barranco <danielbarranco@gmail.com>
2016-08-15 12:50:48 -0400

    init repo

---
+++ js/tweetModule.js
@@ -0,0 +1,36 @@
+var tweetApp = angular.module('tweetApp', ['ngRoute']);
+
+tweetApp.directive('tweetHello', function(){
+	return{
+		restrict: 'E', //E = Element, A = Attribute, C = Class, M = Comment
+		template: "Hello Class!"
+	}
+});
+
+tweetApp.directive('helloAttr', function(){
+	return{
+		restrict: 'A', //E = Element, A = Attribute, C = Class, M = Comment
+		// template: "Hello Class from an attribute {{test}}",
+		templateUrl: "views/hello.html"
+	}
+});
+
+tweetApp.directive('helloClass', function(){
+	return{
+		restrict: 'C', //E = Element, A = Attribute, C = Class, M = Comment
+		template: "Hello Class from another type of class"
+	}
+});
+
+tweetApp.directive('turnMeBlue', function(){
+	return{
+		link: function($scope, element, attrs){
+			element.bind('mouseenter', function(){
+				element.css('color', 'blue');
+			});
+			element.bind('mouseleave', function(){
+				element.css('color', 'black');
+			});
+		}
+	}
+});
